e77229f81a37216b1ba3700ab76f0a51f79d2e3e
angie329 <aalfonso@espol.edu.ec>
2025-11-12 05:18:09 -0500

    avance con el index y el js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//calculadora
+const numA = document.getElementById('numA');
+const numB = document.getElementById('numB');
+const resul = document.getElementById('resul');
+const error = document.getElementById('err');
+const buttons = document.querySelectorAll('#calculadora .buttons button');
+
